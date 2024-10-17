9b2b8f8b4a44634170bfd737d030877b52c20bb2
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-17 13:48:34 -0300

    Add IRepository interface for performing CRUD operations on entities

---
+++ backend/src/BudgetBuddy.Domain/Abstractions/Repository/Core/IRepository.cs
@@ -0,0 +1,37 @@
+namespace BudgetBuddy.Domain.Abstractions.Repository.Core;
+
+/// <summary>
+/// Defines a repository interface for performing CRUD operations on entities.
+/// </summary>
+/// <typeparam name="TEntity">The type of the entity.</typeparam>
+public interface IRepository<TEntity> where TEntity : class
+{
+    /// <summary>
+    /// Retrieves an entity by its identifiers.
+    /// </summary>
+    /// <param name="identifiers">The identifiers of the entity.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the entity.</returns>
+    Task<TEntity> GetByIdentifiersAsync(params object[] identifiers);
+
+    /// <summary>
+    /// Adds a new entity to the repository.
+    /// </summary>
+    /// <param name="entity">The entity to add.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the added entity.</returns>
+    Task<TEntity> AddAsync(TEntity entity);
+
+    /// <summary>
+    /// Updates an existing entity in the repository.
+    /// </summary>
+    /// <param name="entity">The entity to update.</param>
+    /// <param name="identifiers">The identifiers of the entity.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the updated entity.</returns>
+    Task<TEntity> UpdateAsync(TEntity entity, params object[] identifiers);
+
+    /// <summary>
+    /// Deletes an entity from the repository by its identifiers.
+    /// </summary>
+    /// <param name="identifiers">The identifiers of the entity.</param>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the deleted entity.</returns>
+    Task<TEntity> DeleteAsync(params object[] identifiers);
+}
