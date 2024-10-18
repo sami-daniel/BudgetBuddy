7a6a9e038a59271ee226902e96049db9c305ecdd
Sami Daniel <130937402+sami-daniel@users.noreply.github.com>
2024-10-18 15:23:06 +0000

    Add DuplicatedEntityException class to handle adding duplicate entities to the repository

--- backend/src/BudgetBuddy.Domain/Abstractions/Repository/Exceptions/DuplicatedEntityException.cs
+++ backend/src/BudgetBuddy.Domain/Abstractions/Repository/Exceptions/DuplicatedEntityException.cs
@@ -1,19 +1,39 @@
 namespace BudgetBuddy.Domain.Abstractions.Repository.Exceptions;
 
+/// <summary>
+/// Exception thrown when an attempt is made to add an entity that already exists in the repository.
+/// </summary>
 public class DuplicatedEntityException : RepositoryException
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DuplicatedEntityException"/> class with a default error message.
+    /// </summary>
     public DuplicatedEntityException() : base("The entity already exists.")
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DuplicatedEntityException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public DuplicatedEntityException(string? message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DuplicatedEntityException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
     public DuplicatedEntityException(string? message, Exception? innerException) : base(message, innerException)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DuplicatedEntityException"/> class with a specified entity type and key.
+    /// </summary>
+    /// <param name="entityType">The type of the entity that already exists.</param>
+    /// <param name="entityKey">The key of the entity that already exists.</param>
     public DuplicatedEntityException(Type entityType, object? entityKey) : base($"The entity of type {entityType.Name} with key {entityKey} already exists.")
     {
     }
--- backend/src/BudgetBuddy.Infrastructure/Repository/Core/Repository.cs
+++ backend/src/BudgetBuddy.Infrastructure/Repository/Core/Repository.cs
@@ -1,4 +1,5 @@
 using BudgetBuddy.Domain.Abstractions.Repository.Core;
+using BudgetBuddy.Domain.Abstractions.Repository.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace BudgetBuddy.Infrastructure.Repository.Core;
@@ -7,8 +8,50 @@ public abstract class Repository<TEntity>(DbContext context) : IRepository<TEnti
 {
     private readonly DbContext _context = context;
 
-    public Task<TEntity> AddAsync(TEntity entity) => throw new NotImplementedException();
+    public async Task<TEntity> AddAsync(TEntity entity)
+    {
+        var entityExists = await _context.Set<TEntity>().AnyAsync(e => e.Equals(entity));
+
+        if (entityExists)
+        {
+            throw new DuplicatedEntityException(typeof(TEntity), GetEntityKey(entity));
+        }
+        
+        try 
+        {
+            await _context.Set<TEntity>().AddAsync(entity);
+        }
+        catch
+        {
+            throw new RepositoryException();
+        }
+
+        return entity;
+    }
     public Task<TEntity> DeleteAsync(params object[] identifiers) => throw new NotImplementedException();
     public Task<TEntity> GetByIdentifiersAsync(params object[] identifiers) => throw new NotImplementedException();
     public Task<TEntity> UpdateAsync(TEntity entity, params object[] identifiers) => throw new NotImplementedException();
+
+    private object? GetEntityKey(TEntity entity)
+    {
+        var keyNames = _context.Model.FindEntityType(typeof(TEntity))?.FindPrimaryKey()?.Properties
+            .Select(x => x.Name).ToList();
+
+        if (keyNames == null || keyNames.Count == 0)
+        {
+            return null;
+        }
+
+        var keyValues = new List<object>();
+        foreach (var keyName in keyNames)
+        {
+            var keyValue = typeof(TEntity).GetProperty(keyName)?.GetValue(entity, null);
+            if (keyValue != null)
+            {
+                keyValues.Add(keyValue);
+            }
+        }
+
+        return keyValues.Count == 1 ? keyValues[0] : Array.Empty<object>();
+    }
 }
