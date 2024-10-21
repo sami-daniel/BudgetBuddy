042efaaa00d61cf07d037818480d8d022b9d3a89
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-21 20:44:23 -0300

    Refactor UnitOfWork class to handle transactions and execute database operations asynchronously

--- backend/src/BudgetBuddy.Infrastructure/UnitOfWork/UnitOfWork.cs
+++ backend/src/BudgetBuddy.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -1,10 +1,49 @@
 using BudgetBuddy.Domain.Abstractions.UnitOfWork;
+using BudgetBuddy.Domain.Abstractions.UnitOfWork.Exceptions;
+using Microsoft.EntityFrameworkCore;
 
 namespace BudgetBuddy.Infrastructure.UnitOfWork;
 
-public class UnitOfWork : IUnitOfWork
+public class UnitOfWork(DbContext context) : IUnitOfWork
 {
-    public Task BeginTransactionAsync() => throw new NotImplementedException();
-    public Task CommitTransactionAsync() => throw new NotImplementedException();
-    public Task<int> ExecuteAsync() => throw new NotImplementedException();
+    private readonly DbContext _context = context;
+
+    public async Task BeginTransactionAsync()
+    {
+        try
+        {
+            await _context.Database.BeginTransactionAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new TransactionException("An error occurred while starting the transaction.", ex);
+        }
+    }
+
+    public async Task CommitTransactionAsync()
+    {
+        var currentTransaction = _context.Database.CurrentTransaction
+                                 ?? throw new TransactionException("Transaction has not been started.");
+
+        try
+        {
+            await currentTransaction.CommitAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new TransactionException("An error occurred while committing the transaction.", ex);
+        }
+    }
+
+    public async Task<int> ExecuteAsync()
+    {
+        try
+        {
+            return await _context.SaveChangesAsync();
+        }
+        catch (Exception ex)
+        {
+            throw new ExecuteException("An error occurred while saving changes.", ex);
+        }
+    }
 }
