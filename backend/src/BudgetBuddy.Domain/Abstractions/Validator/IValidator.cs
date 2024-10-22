1c226593e81c68839952c6477055fe1817fd3540
Sami Daniel <samidaniel125@gmail.com>
2024-10-22 09:05:31 -0300

    Create IValidator interface for represents validation process under a generic Entity

---
+++ backend/src/BudgetBuddy.Domain/Abstractions/Validator/IValidator.cs
@@ -0,0 +1,12 @@
+﻿using BudgetBuddy.Domain.Entities;
+
+namespace BudgetBuddy.Domain.Abstractions.Validator;
+
+/// <summary>
+/// Represents a validation process of an <see cref="{TEntity}"/> entity.
+/// </summary>
+/// <typeparam name="TEntity">The entity to be validated.</typeparam>
+public interface IValidator<TEntity> where TEntity : class
+{
+    Task<ValidationState> ValidateAsync(TEntity entity);
+}
