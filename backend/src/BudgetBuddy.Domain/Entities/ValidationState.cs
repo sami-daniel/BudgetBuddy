a58767d33bfa2a34732210eacd377c169eabea15
Sami Daniel <samidaniel125@gmail.com>
2024-10-22 09:01:39 -0300

    Create ValidationState poco class

---
+++ backend/src/BudgetBuddy.Domain/Entities/ValidationState.cs
@@ -0,0 +1,17 @@
+﻿namespace BudgetBuddy.Domain.Entities;
+
+/// <summary>
+/// Represents the actual state of an validation.
+/// </summary>
+public class ValidationState
+{
+    /// <summary>
+    /// Gets the actual validation state
+    /// </summary>
+    public virtual bool IsSucessfulValidation { get; }
+
+    /// <summary>
+    /// Gets the error messages provided by the validation process.
+    /// </summary>
+    public virtual IEnumerable<string> ErrorMessages { get; } = new List<string>();
+}
