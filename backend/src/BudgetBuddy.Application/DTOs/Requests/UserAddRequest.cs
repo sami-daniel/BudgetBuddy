aa6b52d89914ac9976a562ff0be5dbe640797bb6
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-22 21:25:26 -0300

    Refactor UserAddRequest class to include properties for username and password

--- backend/src/BudgetBuddy.Application/DTOs/Requests/UserAddRequest.cs
+++ backend/src/BudgetBuddy.Application/DTOs/Requests/UserAddRequest.cs
@@ -1,7 +1,17 @@
 namespace BudgetBuddy.Application.DTOs.Requests;
 
+/// <summary>
+/// Represents a request to add a new user to the BudgetBuddy application.
+/// </summary>
 public class UserAddRequest
 {
+    /// <summary>
+    /// Gets or sets the username of the user.
+    /// </summary>
     public string Username { get; set; } = null!;
+
+    /// <summary>
+    /// Gets or sets the password of the user.
+    /// </summary>
     public string UserPassword { get; set; } = null!;
 }
