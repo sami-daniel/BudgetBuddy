29f126f5c58fde4cd2539bc9dafd1778a564b810
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-22 21:24:17 -0300

    Refactor User class properties to remove unnecessary "required" attribute

--- backend/src/BudgetBuddy.Domain/Entities/User.cs
+++ backend/src/BudgetBuddy.Domain/Entities/User.cs
@@ -8,20 +8,20 @@ public class User
     /// <summary>
     /// Gets or sets the unique identifier for the user.
     /// </summary>
-    public virtual required Guid UserId { get; set; }
+    public virtual Guid UserId { get; set; }
 
     /// <summary>
     /// Gets or sets the username of the user.
     /// </summary>
-    public virtual required string Username { get; set; }
+    public virtual string Username { get; set; } = null!;
 
     /// <summary>
     /// Gets or sets the password of the user.
     /// </summary>
-    public virtual required string UserPassword { get; set; }
+    public virtual string UserPassword { get; set; } = null!;
 
     /// <summary>
     /// Gets or sets the date and time when the user was created.
     /// </summary>
-    public virtual required DateTime CreatedAt { get; set; }
+    public virtual DateTime CreatedAt { get; set; }
 }
