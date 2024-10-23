0ffd3005e0e2eb119068da090ffa8c516c0d1136
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-23 19:18:21 -0300

    Refactor UserAddRequest class to override ToString, Equals, and GetHashCode methods

--- backend/src/BudgetBuddy.Application/DTOs/Requests/UserAddRequest.cs
+++ backend/src/BudgetBuddy.Application/DTOs/Requests/UserAddRequest.cs
@@ -14,4 +14,24 @@ public class UserAddRequest
     /// Gets or sets the password of the user.
     /// </summary>
     public string UserPassword { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"Username: {Username}, UserPassword: {UserPassword}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not UserAddRequest userAddRequest)
+        {
+            return false;
+        }
+
+        return Username == userAddRequest.Username && UserPassword == userAddRequest.UserPassword;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Username, UserPassword);
+    }
 }
