28e7bba995fc840bbcb5cb6494378eefa2b38144
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-23 18:03:37 -0300

    Refactor UserService class to use dependency injection for unit of work, mapper, and user validator

--- backend/src/BudgetBuddy.Application/Services/Flow/Implementations/UserService.cs
+++ backend/src/BudgetBuddy.Application/Services/Flow/Implementations/UserService.cs
@@ -1,11 +1,19 @@
-﻿using BudgetBuddy.Application.DTOs.Requests;
+﻿using AutoMapper;
+using BudgetBuddy.Application.DTOs.Requests;
 using BudgetBuddy.Application.DTOs.Responses;
 using BudgetBuddy.Application.Services.Flow.Abstractions;
+using BudgetBuddy.Domain.Abstractions.UnitOfWork;
+using BudgetBuddy.Domain.Abstractions.Validator;
+using BudgetBuddy.Domain.Entities;
 
 namespace BudgetBuddy.Application.Services.Flow.Implementations;
 
-public class UserService : IUserService
+public class UserService(IUnitOfWork unitOfWork, IMapper mapper, IValidatable<User> validatableUser) : IUserService
 {
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IMapper _mapper = mapper;
+    private readonly IValidatable<User> _validatableUser = validatableUser;
+
     public Task<UserResponse?> GetUserByIDAsync(Guid userId) => throw new NotImplementedException();
     public Task<UserResponse> RegisterAsync(UserAddRequest userAddRequest) => throw new NotImplementedException();
 }
