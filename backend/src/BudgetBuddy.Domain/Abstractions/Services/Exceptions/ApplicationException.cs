56060cfc9b8d2313d64b429ce225a28a8429055f
Sami Daniel Santos Silva <samidaniel125@gmail.com>
2024-10-23 18:14:54 -0300

    Add ApplicationException class to handle application-specific exceptions

---
+++ backend/src/BudgetBuddy.Domain/Abstractions/Services/Exceptions/ApplicationException.cs
@@ -0,0 +1,16 @@
+namespace BudgetBuddy.Domain.Abstractions.Services.Exceptions;
+
+public class ApplicationException : Exception
+{
+    public ApplicationException() : base()
+    {
+    }
+
+    public ApplicationException(string message) : base(message)
+    {
+    }
+
+    public ApplicationException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
