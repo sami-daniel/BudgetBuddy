d6063260a7ec92abf6e601ad8af517ddfde6c272
Sami Daniel <samidaniel125@gmail.com>
2024-10-23 11:57:58 -0300

    Refactor Program.cs to improve code structure and add layered services

--- backend/src/BudgetBuddy.WebAPI/Program.cs
+++ backend/src/BudgetBuddy.WebAPI/Program.cs
@@ -1,44 +1,37 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿using BudgetBuddy.WebAPI.Services;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+namespace BudgetBuddy.WebAPI;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+        // The container DI is configured by the IoC project to isolate the WebAPI project from the rest of the application
+        builder.Services.AddLayeredServices();
+        var app = builder.Build();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+        // Configure the HTTP request pipeline.
+#if DEBUG
+        app.UseSwagger();
+        app.UseSwaggerUI();
+#endif
 
-app.Run();
+#if DEBUG
+        if (app.Environment.IsProduction())
+        {
+            app.UseHttpsRedirection();
+        }
+#endif
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        app.Run();
+
+    }
 }
