597027161785088fb2db059fd1b44807a78a8b9f
Sami Daniel <130937402+sami-daniel@users.noreply.github.com>
2024-10-24 11:46:21 +0000

    Refactor BudgetBuddy.WebAPI.csproj to generate documentation file

--- backend/src/BudgetBuddy.WebAPI/Controllers/UsersController.cs
+++ backend/src/BudgetBuddy.WebAPI/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 using BudgetBuddy.Application.Services.Flow.Abstractions;
+using BudgetBuddy.Application.DTOs.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BudgetBuddy.WebAPI.Controllers;
@@ -9,6 +10,13 @@ public class UsersController(IUserService userService) : ControllerBase
 {
     private readonly IUserService _userService = userService;
 
+    /// <summary>
+    /// Gets the user corresponding to the provided name.
+    /// </summary>
+    /// <param name="userID">Name of the user to be retrieved.</param>
+    /// <returns>A <see cref="UserResponse"/> object representing the found user.</returns>
+    /// <response code="200">User successfully found.</response>
+    /// <response code="404">User with the userID not found.</response>
     [HttpGet("{userID:guid}")]
     public async Task<IActionResult> GetUserByID(Guid userID)
     {
